7fcb51bc310fb4732a2866f709178e69688bd8f9
Vitor Dev <80135764+VitorrDevX@users.noreply.github.com>
2021-11-06 15:37:44 -0300

    Create fxmanifest.lua

---
+++ Desafio-4/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+game 'gta5'
+
+shared_script 'cfg.lua'
+
+server_scripts {
+    "@vrp/lib/utils.lua",
+    'server.lua',
+}
+client_scripts {
+    "@vrp/lib/utils.lua",
+    'client.lua',
+}
