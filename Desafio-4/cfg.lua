ce7ed7455b593cfe20b6c2f8eafc375540a8d68b
Vitor Dev <80135764+VitorrDevX@users.noreply.github.com>
2021-11-06 15:37:20 -0300

    Create cfg.lua

---
+++ Desafio-4/cfg.lua
@@ -0,0 +1,15 @@
+cfg = {}
+
+cfg.locs = { -- Coloque a localização de todos os radares neste local
+    vec3(-2002.5313720703,3292.5261230469,32.988464355469), 
+}
+
+cfg.multaMin = 1000 -- Coloque o valor de multa minimo para ser aplicado
+cfg.multaMax = 2000 -- Coloque o valor de multa maximo para ser aplicado
+
+cfg.velocidade = 50.0 -- Coloque a velocidade que deve ser ultrapassada para aplicar a multa
+
+cfg.blipType = 42 -- Coloque o tipo do blip
+cfg.blipColor = 75 -- Coloque a cor do blip
+cfg.blipScale = 0.5 -- Coloque a scala do blip
+cfg.blipName = 'Radar' -- Coloque o nome que o blip irá aparecer no mapa
